be43b09053a29ccd2414c65a1f5213c6a0821107
Hemant <hrsmart2150@gmail.com>
2023-07-14 17:31:07 +0530

    formatted the code a little

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -5,20 +5,8 @@ import { Check, ChevronsUpDown, PlusCircle, Store } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-} from "@/components/ui/command"
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "@/components/ui/command"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { useParams, useRouter } from "next/navigation"
 
