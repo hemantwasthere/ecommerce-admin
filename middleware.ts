c46765cb101c20b3607167e38d1de5727b0f62d4
Hemant <hrsmart2150@gmail.com>
2024-04-14 22:59:35 +0530

    Added ignoredRoutes configuration to authMiddleware in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/:path*", "/"], // Public routes that don't require authentication
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/"],
 });
 
 export const config = {
