28795077128fe00e8e62c83c7652a15e384d6cfb
Hemant <hrsmart2150@gmail.com>
2024-04-14 22:54:28 +0530

    Added "/" in public routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"], // Public routes that don't require authentication
-})
+  publicRoutes: ["/api/:path*", "/"], // Public routes that don't require authentication
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
